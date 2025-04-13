a49007b7b6c15600f5b5a773d7c48c2c96be5403
mohd.zaki <mohd.zaki@impact-multimedia.com>
2025-04-13 15:32:22 +0800

    initial commit: left off at episode 5

---
+++ resources/views/ninjas/index.blade.php
@@ -0,0 +1,27 @@
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+        >
+        <title>Ninja Network | Home</title>
+    </head>
+
+    <body>
+
+        <h2>Currently Available Ninjas</h2>
+
+        <ul>
+            @foreach($ninjas as $ninja)
+                <li>
+                    <p>{{ $ninja['name'] }}</p>
+                    <a href="/ninjas/{{ $ninja['id'] }}">View Details</a>
+                </li>
+            @endforeach
+        </ul>
+
+    </body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/ninjas', function () {
+    $ninjas = [["name" => "mario", "skill" => 75, "id" => "1"], ["name" => "zaki", "skill" => 100, "id" => "2"]];
+
+    return view('ninjas.index', ["greeting" => "hello", "ninjas" => $ninjas]);
+});
+
+Route::get('/ninjas/{id}', function ($id) {
+    return view('ninjas.show', ["id" => $id]);
+});
