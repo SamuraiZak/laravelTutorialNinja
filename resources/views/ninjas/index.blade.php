19be5c9a0d2df3c2bd7b4d162cd2ca1ec832d808
mohd.zaki <mohd.zaki@impact-multimedia.com>
2025-04-13 16:08:35 +0800

    using card component, passing attributes, props and conditional classes

--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -1,27 +1,13 @@
-<html lang="en">
+<x-layout>
+    <h2>Currently Available Ninjas</h2>
 
-    <head>
-        <meta charset="UTF-8">
-        <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-        >
-        <title>Ninja Network | Home</title>
-    </head>
-
-    <body>
-
-        <h2>Currently Available Ninjas</h2>
-
-        <ul>
-            @foreach($ninjas as $ninja)
-                <li>
-                    <p>{{ $ninja['name'] }}</p>
-                    <a href="/ninjas/{{ $ninja['id'] }}">View Details</a>
-                </li>
-            @endforeach
-        </ul>
-
-    </body>
-
-</html>
+    <ul>
+        @foreach ($ninjas as $ninja)
+            <li>
+                <x-card href="/ninjas/{{ $ninja['id'] }}" :highlight="$ninja['skill'] > 70">
+                    <h3>{{ $ninja['name'] }}</h3>
+                </x-card>
+            </li>
+        @endforeach
+    </ul>
+</x-layout>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,4 @@
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-        >
-        <title>Ninja Network</title>
-    </head>
-
-    <body>
+<x-layout>
         <h1>Welcome Ninja</h1>
         <p>Click the button below to view the list of ninjas.</p>
 
@@ -17,6 +6,5 @@
             href="/ninjas"
             class="btn"
         >Find Ninjas</a>
-    </body>
 
-</html>
+</x-layout>
